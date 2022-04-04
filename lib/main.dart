6e952251183d9784e5a632e7060e66574988790b
hweij <henk000@gmail.com>
2022-04-04 14:59:29 +0200

    feat: update app icon and theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
